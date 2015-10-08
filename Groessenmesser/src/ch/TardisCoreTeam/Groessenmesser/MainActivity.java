3b978be56f1838d47b4b9aebf728b1c808b51edd
WalterWolf49 <zippo.burn@gmx.ch>
2015-10-08 08:12:02 +0200

    -

--- Groessenmesser/src/ch/TardisCoreTeam/Groessenmesser/MainActivity.java
+++ Groessenmesser/src/ch/TardisCoreTeam/Groessenmesser/MainActivity.java
@@ -3,13 +3,13 @@ package ch.TardisCoreTeam.Groessenmesser;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
